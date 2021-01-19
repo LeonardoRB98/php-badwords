7f453bb5e80de286b31bf4f75c6c422961f9864b
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-01-19 16:18:03 +0100

    commint 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$paragrafo = str_replace($badWord, '***', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+$badWord = $_GET['censura'];
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BadWords</title>
+  </head>
+  <body>
+    <h1>parola da sostituire: <?php echo $badWord?></h1>
+    <p><?php echo $paragrafo; ?></p>
+    <h2>Lunghezza:<?php echo strlen($paragrafo); ?></h2>
+  </body>
+</html>
